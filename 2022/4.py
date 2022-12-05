71d7877af385c92d4d0c4a3f393b3c6ef451dbaf
Mark Frankli <mark@pearscom.com>
2022-12-05 23:30:05 +0100

    1-liner day 4

--- 2022/4.py
+++ 2022/4.py
@@ -1,18 +1,10 @@
 intervals = [[[int(z) for z in x.split('-')] for x in y.rstrip().split(',')] for y in open('4.txt').readlines()]
 
 def first_part(intervals):
-  cnt = 0
-  for line in intervals:
-    if (line[0][0] <= line[1][0] and line[0][1] >= line[1][1]) or (line[1][0] <= line[0][0] and line[1][1] >= line[0][1]):
-      cnt += 1
-  return cnt
+  return sum((1 if ((l[0][0] <= l[1][0] and l[0][1] >= l[1][1]) or (l[1][0] <= l[0][0] and l[1][1] >= l[0][1])) else 0) for l in intervals)
 
 def second_part(intervals):
-  cnt = 0
-  for line in intervals:
-    if not (line[1][1] < line[0][0] or line[0][1] < line[1][0]):
-      cnt += 1
-  return cnt
+  return sum((1 if not (l[1][1] < l[0][0] or l[0][1] < l[1][0]) else 0) for l in intervals)
 
 print('First part:', first_part(intervals))
 print('Second part:', second_part(intervals))
