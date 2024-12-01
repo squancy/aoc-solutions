f48e6cf0d8009b1e9ebbc13288a516202cbadbd9
Mark Frankli <mark@pearscom.com>
2024-12-01 16:08:57 +0100

    added first day

---
+++ 2024/1.py
@@ -0,0 +1,12 @@
+f = open('1.txt').readlines()
+a = sorted([int(x.split('   ')[0]) for x in f])
+b = sorted([int(x.split('   ')[1]) for x in f])
+
+def first_part():
+    return sum([abs(a[i] - b[i]) for i in range(len(a))])
+
+def second_part():
+    return sum([b.count(x) * x for x in a])
+
+print(first_part())
+print(second_part())
